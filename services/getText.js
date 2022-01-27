b0522fdd69a785cdb4f589d85e4e00973076b978
Sebastian Gil <shiuxcode@gmail.com>
2022-01-26 21:40:42 -0500

    Fix text

--- services/getText.js
+++ services/getText.js
@@ -1,5 +1,5 @@
 const TEXT =
-  "Mientras caminaba por"
+  "Mientras caminaba por el bosque, un lobo la vigilaba escondido entre unos matorrales, era un lobo muy grande y feroz, y tenía una boca muy grande, con la que podía comerse a Caperucita Roja, pero no se atrevió a hacerlo, debido a que unos leñadores estaban trabajando en el bosque."
 
 export default async function getText() {
   const arrayText = TEXT.split(" ").map(item => item + " ")
