de077d34c825a6511fa4313a873465ac4e23764e
Sebastian Gil <shiuxcode@gmail.com>
2022-02-08 21:27:57 -0500

    Add handle return

--- components/MenuGamePage/index.jsx
+++ components/MenuGamePage/index.jsx
@@ -1,17 +1,27 @@
+import { useRouter } from 'next/router';
+;
+
+
 import styles from "./styles"
 import Repeat from "icons/Repeat"
 import Return from "icons/Return"
 
 export default function MenuGamePage() {
+  const router = useRouter();
+
   const handleRepeat = () => {
     console.log("Repeat")
   }
+  const handleReturn = () => {
+    router.push('/levels')
+  }
+
   return (
     <>
       <nav>
         <ul>
           <li>
-            <button>
+            <button onClick={handleReturn}>
               <Return height='25' width='25' />
             </button>
           </li>
